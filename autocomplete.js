acf9b22e2eeff487b8be264406c6d635ca010a83
Balaji367 <balajikd367@gmail.com>
2020-08-25 19:20:26 +0530

    Update autocomplete.js

--- autocomplete.js
+++ autocomplete.js
@@ -32,7 +32,7 @@ const autoComplete = ({ root, renderOption, onOptionSelect, inputValue, fetchDat
             resultsWrapper.append(option);
         }
     }
-    input.addEventListener('input', debounce(onInput,500));
+    input.addEventListener('input', debounce(onInput,200));
     document.addEventListener("click", event =>
     {
         if(!root.contains(event.target))
